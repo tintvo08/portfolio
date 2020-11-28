06d98d42930be9bb963bee4d7794ade25a16e121
tin <tintvo08@gmail.com>
2020-11-27 19:46:20 -0800

    adding earnings and phases page

--- jobs/views.py
+++ jobs/views.py
@@ -13,6 +13,18 @@ def home(request):
     return render(request, 'jobs/home.html', {'earnings': earnings})
 
 
+def earnings(request):
+    upcoming = _get_upcoming()
+    earnings = upcoming['earnings']
+    return render(request, 'jobs/earnings.html', {'earnings': earnings})
+
+
+def phases(request):
+    upcoming = _get_upcoming()
+    phases = upcoming['phases']
+    return render(request, 'jobs/earnings.html', {'phases': phases})
+
+
 def _get_upcoming():
     headers = {'api-key': os.getenv("TRADE_API_KEY")}
     resp = requests.get('https://t-trade-api.herokuapp.com/t_trade/upcoming', headers=headers)
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
 import jobs.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', jobs.views.home, name='jobs/home')
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', jobs.views.home, name='jobs/home'),
+    path('earnings', jobs.views.earnings, name='/jobs/earnings')
+]
