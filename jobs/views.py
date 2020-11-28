3e0849b148c826df6e93b524b639018fbf698061
tin <tintvo08@gmail.com>
2020-11-27 19:11:43 -0800

    adding earnings page

--- jobs/views.py
+++ jobs/views.py
@@ -1,9 +1,24 @@
+import requests
+import os
+
 from django.shortcuts import render
-from .models import Job
+from dotenv import load_dotenv
+load_dotenv()
 
 
 def home(request):
-    jobs = Job.objects
-    return render(request, 'jobs/home.html', {'jobs': jobs})
+    upcoming = _get_upcoming()
+    earnings = upcoming['earnings']
+    phases = upcoming['phases']
+    return render(request, 'jobs/home.html', {'earnings': earnings})
+
+
+def _get_upcoming():
+    headers = {'api-key': os.getenv("TRADE_API_KEY")}
+    resp = requests.get('https://t-trade-api.herokuapp.com/t_trade/upcoming', headers=headers)
+    data = resp.json()
+    return data
+
+
 
 
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -40,8 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'jobs.apps.JobsConfig',
-    'blog.apps.BlogConfig'
+    'jobs.apps.JobsConfig'
 ]
 
 MIDDLEWARE = [
