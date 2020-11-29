379544b67a3f74954c80ca6fea8cface1564216f
tin <tintvo08@gmail.com>
2020-11-29 15:46:18 -0800

    added allowed host

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY_PORTFOLIO")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://tin-profile.herokuapp.com', '127.0.0.1']
 
