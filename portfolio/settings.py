bfdf5479d4c8fb767e9b6b053e1c61a385c0327e
tin <tintvo08@gmail.com>
2020-11-29 16:52:05 -0800

    updating page

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY_PORTFOLIO")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['tin-profile.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['tin-profile.herokuapp.com', 'localhost']
 
 
 # Application definition
